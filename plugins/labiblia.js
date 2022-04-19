124025468f2735d887f715b69e1034ce1b3d704d
ixi666 <103534276+ixi666@users.noreply.github.com>
2022-04-19 10:58:48 -0600

    isi

--- plugins/labiblia.js
+++ plugins/labiblia.js
@@ -9,14 +9,14 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let username = conn.getName(who)
 let vn = './media/mariana.mp3'
 let menu =`
-╭══〘 ✯✯✯✯✯✯✯✯ 〙═╮
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-║➤ *✨𝗛ola, ${username}!!*
-║≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-╰══╡✯✯✯✯✯✯✯✯╞══╯
+╭══〘 
+║
+║ *✨𝗛ola, ${username}!!*
+║
+╰══╡
 ┏━━━━━━━━━━━━━┓
-┃ *<MENU +18/>*
-┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+┃ *MENU +18*
+┃
 ┣ ඬ⃟🔞 _${usedPrefix}video_
 ┣ ඬ⃟🔞 _${usedPrefix}imglesbians_
 ┣ ඬ⃟🔞 _${usedPrefix}porno_
@@ -58,7 +58,7 @@ let menu =`
 ┣ ඬ⃟🔞 _${usedPrefix}nsfwloli_
 ┗━━━━━━━━━━━━━┛`.trim()
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, pp, menu, '©The Shadow Brokers - Bot', '𝕄 𝔼 ℕ 𝕌   ℙ ℝ 𝕀 ℕ ℂ 𝕀 ℙ 𝔸 𝕃', `#menu`, m, false, { contextInfo: { mentionedJid }})   
+conn.sendButtonImg(m.chat, pp, menu, '©The ixxi - Bot', '𝕄 𝔼 ℕ 𝕌   ℙ ℝ 𝕀 ℕ ℂ 𝕀 ℙ 𝔸 𝕃', `#menu`, m, false, { contextInfo: { mentionedJid }})   
 await await await await await await conn.sendFile(m.chat, vn, 'ora.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
